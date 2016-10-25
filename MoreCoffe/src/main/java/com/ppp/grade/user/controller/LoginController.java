e51c18ff080d4c02839fd6d5192a088f0e870dfd
성아 <id6827@naver.com>
2016-10-26 05:30:30 +0900

    LoginController 수정
    불필요한 부분 삭제

--- MoreCoffe/src/main/java/com/ppp/grade/user/controller/LoginController.java
+++ MoreCoffe/src/main/java/com/ppp/grade/user/controller/LoginController.java
@@ -17,8 +17,6 @@ public class LoginController implements Controller {
 		
 		String num = request.getParameter("username");
 		String password = request.getParameter("password");
-		System.out.println(num);
-		System.out.println(password);
 		UserVO vo = new UserVO();
 		vo.set학번(num);
 		vo.set패스워드(password);
@@ -28,38 +26,9 @@ public class LoginController implements Controller {
 		
 		ModelAndView mav = new ModelAndView();
 		if(user != null){
-			/*//MVC_2 response.sendRedirect("sub.jsp");
-			System.out.println("sub.jsp����");
-			//return "sub";
-			if(user.get().isEmpty() && user.getGeneric().isEmpty() && user.getNecessaries().isEmpty() && user.getLiberal().isEmpty() ){
-				mav.setViewName("sub.jsp");
-			}
-			else{
-				if (!user.getMajor().isEmpty()) {
-					int score = 0;
-					String subject = ""; 
-					String[] str = user.getMajor().split(",");
-					for(String subject_num : str){
-						MajorVO obj = new MajorVO();
-						obj.setNum(subject_num);
-						MajorDAO majorDAO = new MajorDAO();
-						MajorVO grade = majorDAO.getMajor(obj);
-						//System.out.println(grade.getName() + grade.getGrade());
-						score += Integer.parseInt(grade.getGrade());
-						subject += ", " + grade.getName();
-					}
-					System.out.println(score);
-					System.out.println(subject);
-				}
-				mav.setViewName("getMajorList.do");
-				//System.out.println("�ϳ��� ������");
-			}*/
-			System.out.println("ㅁㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ");
+			mav.setViewName("select.jsp");
 		}
 		else{
-			//MVC_2 response.sendRedirect("login.jsp");
-			System.out.println("login.jsp");
-			//return "login";
 			mav.setViewName("login.jsp");
 		}
 		return mav;
