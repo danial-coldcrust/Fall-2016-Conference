bcc3ca09cf1727300d45c391e51541942211cbc0
성아 <id6827@naver.com>
2016-10-26 18:29:50 +0900

    성아파일
    123

---
+++ MoreCoffee/src/main/java/com/ppp/grade/user/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.ppp.grade.user.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.ppp.grade.user.persistence.UserDAO;
+import com.ppp.grade.user.persistence.UserVO;
+
+public class UserController implements Controller {
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		/*String num = request.getParameter("num");
+		UserVO vo = new UserVO();
+		vo.set학번(num);*/
+		
+		UserDAO userDAO = new UserDAO();
+		UserVO vo = userDAO.getUser(null);
+		
+		List<UserVO> userList = userDAO.getUserList(vo);
+		for (UserVO tmp : userList){
+			System.out.println(tmp);
+		}
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("userList",userList);
+		mav.setViewName("");
+		return mav;
+	}
+}
---
+++ MoreCoffee/src/main/java/com/ppp/grade/user/persistence/UserDAO.java
@@ -0,0 +1,126 @@
+package com.ppp.grade.user.persistence;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.ppp.grade.common.JDBCUtil;
+
+
+
+
+@Repository("userDAO")
+public class UserDAO {
+
+   private Connection conn = null;
+   private PreparedStatement stmt = null;
+   private ResultSet rs = null;
+   private final String USER_INSERT = "insert into STUDENT(학번, 이름, 패스워드, 주전공, 복수전공, 입학년도) values(?,?,?,?,?,?)";
+   private final String USER_GET = "select * from STUDENT where 학번=? and 패스워드=?";
+   private final String USER_DELETE = "delete STUDENT where 학번 = ?";
+   private final String USER_UPDATE = "update STUDENT set 이름 = ?, 패스워드 =?, 주전공 =?, 복수전공=?, 입학년도 =? where 학번 = ?";
+   private final String USER_LIST = "select * from STUDENT order by 학번 desc";
+   
+   public void insertUser(UserVO vo){
+      System.out.println("===> JEBC insertUser()");
+      try{
+         conn = JDBCUtil.getConnection();
+         stmt = conn.prepareStatement(USER_INSERT);
+         stmt.setString(1, vo.get학번());
+         stmt.setString(2, vo.get이름());
+         stmt.setString(3, vo.get패스워드());
+         stmt.setString(4, vo.get주전공());
+         stmt.setString(5, null);
+         stmt.setString(6, vo.get학번().substring(0, 4));
+         stmt.executeUpdate();
+      }catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         JDBCUtil.close(stmt, conn);
+      }
+   }
+   public UserVO getUser(UserVO vo){
+      UserVO user = null;
+      try{
+         System.out.println("===> JDBC getUser()");
+         conn = JDBCUtil.getConnection();
+         stmt = conn.prepareStatement(USER_GET);
+         stmt.setString(1,  vo.get학번());
+         stmt.setString(2, vo.get패스워드());
+         rs = stmt.executeQuery();
+         if ( rs.next()){
+            user = new UserVO();
+            user.set학번(rs.getString("학번"));
+            user.set이름(rs.getString("이름"));
+            user.set패스워드(rs.getString("패스워드"));
+            user.set주전공(rs.getString("주전공"));
+            user.set복수전공(rs.getString("복수전공"));
+            user.set입학년도(rs.getString("입학년도"));
+         }
+      }catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         JDBCUtil.close(rs, stmt, conn);
+      }
+      return user;
+   }
+   public void updateUser(UserVO vo){
+	   System.out.println("===> JEBC updateUser()");
+	   try{
+		   conn = JDBCUtil.getConnection();
+		   stmt = conn.prepareStatement(USER_UPDATE);
+		   stmt.setString(1, vo.get이름());
+		   stmt.setString(2, vo.get패스워드());
+		   stmt.setString(3, vo.get주전공());
+		   stmt.setString(4, vo.get복수전공());
+		   stmt.setString(5, vo.get입학년도());
+		   stmt.setString(6, vo.get학번());
+		   stmt.executeUpdate();
+	   }catch(Exception e){
+		   e.printStackTrace();
+	   }finally{
+		   JDBCUtil.close(stmt, conn);
+	   }
+   }
+   public void deleteUser(UserVO vo){
+	   System.out.println("===> JEBC deleteUser()");
+	      try{
+	         conn = JDBCUtil.getConnection();
+	         stmt = conn.prepareStatement(USER_DELETE);
+	         stmt.setString(1, vo.get학번());
+	         stmt.executeUpdate();
+	      }catch(Exception e){
+	         e.printStackTrace();
+	      }finally{
+	         JDBCUtil.close(stmt, conn);
+	      }
+   }
+   public List<UserVO> getUserList(UserVO vo){
+	   System.out.println("===> JEBC getUserList()");
+	   List<UserVO> userList = new ArrayList<UserVO>();
+	   try{
+		   conn = JDBCUtil.getConnection();
+		   stmt = conn.prepareStatement(USER_LIST);
+		   rs = stmt.executeQuery();
+		   while(rs.next()){
+			   UserVO user = new UserVO();
+			   user.set학번(rs.getString("학번"));
+			   user.set이름(rs.getString("이름"));
+			   user.set패스워드(rs.getString("패스워드"));
+			   user.set주전공(rs.getString("주전공"));
+			   user.set복수전공(rs.getString("복수전공"));
+			   user.set입학년도(rs.getString("입학년도"));
+			   userList.add(user);
+		   }
+	   }catch(Exception e){
+		   e.printStackTrace();
+	   }finally{
+		   JDBCUtil.close(stmt, conn);
+	   }
+	   return userList;
+   }   
+}
